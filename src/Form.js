7906b2784fd253348f374807df9d3429619add11
Houcine <106538937+Hcn-88@users.noreply.github.com>
2023-01-02 18:01:47 -0300

    First Commit

---
+++ src/Form.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import Table from "./Table";
+
+const Form = ()  => {
+
+  const [editedContact, setEditedContact] = useState({})
+  const [show, setShow] = useState(false)
+  const [contacts, setContacts] = useState([]);
+  const [state, setState] = useState([{
+    name: "",
+    email: "",
+    age: "",
+    gender: ""
+  }]);
+
+  const getData = (event) => {
+    let fieldName = event.target.getAttribute("name");
+    let fieldValue = event.target.value;
+    const newContact = {...state}
+    newContact[fieldName] = fieldValue;
+    setState(newContact);
+  }
+  
+  const postData = (event) => {
+    event.preventDefault();
+    setContacts(prev => [...prev, state])
+    setState({
+      name: "",
+      email: "",
+      age: "",
+      gender: ""
+    });
+  };
+
+  const removeContact = (ind) => {
+    let updatedContact = contacts.filter((obj, key) => (
+      key !== ind
+    ));
+    setContacts(updatedContact);
+  }
+
+  const contactSelected = (ind) => {
+    setState(contacts[ind]);
+    setShow(!show)
+    setEditedContact(ind)
+  };
+
+  const updateContact = () => {
+    // editedContacted => the targeted index
+    // 1 => the total of elements
+    // state => the replacement of the targeted index (editedContact)
+    contacts.splice(editedContact, 1, state);
+    setContacts([...contacts]);
+    setShow(!show);
+    setState({
+      name: "",
+      email: "",
+      age: "",
+      gender: ""
+    });
+  }
+
+  return (
+    <div className="container">
+      <form className="w-50 mx-auto" autoComplete="off" onSubmit={postData}>
+        <input
+          className="form-control mt-3"
+          type="text"
+          name="name"
+          value={state.name}
+          placeholder="Name..."
+          onChange={getData}
+          required
+        />
+        <input
+          className="form-control my-3"
+          type="text"
+          name="email"
+          value={state.email}
+          placeholder="Email..."
+          onChange={getData}
+          required
+        />
+        <input
+          className="form-control"
+          type="number"
+          name="age"
+          value={state.age}
+          placeholder="Age..."
+          onChange={getData}
+          required
+        />
+        <input
+          className="form-control my-3"
+          type="text"
+          name="gender"
+          value={state.gender}
+          placeholder="Gender..."
+          onChange={getData}
+          required
+        />
+        {!show ? (
+          <button className="btn btn-secondary d-block w-100" type="submit">
+            Submit
+          </button>
+        ) : (
+          ""
+        )}
+      </form>
+      { show ?
+      <button className="btn btn-info d-block w-50 mx-auto" onClick={updateContact}>
+        Update
+      </button> : "" }
+      <Table
+        contacts={contacts}
+        deleteContact={removeContact}
+        selectContact={contactSelected}
+      />
+    </div>
+  );
+}
+
+export default Form;
---
+++ src/Table.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Table = (props) => {
+
+  const { contacts, deleteContact, selectContact } = props;
+
+  return (
+    <div>
+      <table className="table table-striped table-bordered text-center mt-5">
+        <thead className="table-light">
+          <tr>
+            <th>Full Name</th>
+            <th>Email Address</th>
+            <th>Age</th>
+            <th>Gender</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact, index) => (
+            <tr key={index}>
+              <td>{contact.name}</td>
+              <td>{contact.email}</td>
+              <td>{contact.age}</td>
+              <td>{contact.gender}</td>
+              <td className='d-flex justify-content-evenly'>
+                <button
+                  className="btn btn-outline-danger"
+                  onClick={() => deleteContact(index)}
+                >
+                  Delete
+                </button>
+                <button
+                  className='btn btn-outline-warning'
+                  onClick={() => selectContact(index)}>Select</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Table
